e92758b10f3bc43a51eb630bc59018c499d7ba41
tildev <tildevelove@gmail.com>
2019-04-15 00:45:44 +0900

    Add mybatis config

--- ex00/src/test/java/com/tildev/web/MybatisTest.java
+++ ex00/src/test/java/com/tildev/web/MybatisTest.java
@@ -3,11 +3,10 @@
  */
 package com.tildev.web;
 
-import java.sql.Connection;
-
 import javax.inject.Inject;
-import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,24 +14,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author tildev
- * @date 2019. 4. 3.
+ * @date 2019. 4. 15.
  */
-// @RunWith 에러가 뜨면, 
-// 프로젝트 마우스 오른쪽 클릭 -> Build Path -> Configure Build Path... -> Library 탭의 Add Library... 클릭
-// Junit 4 를 추가해 주자.
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/root-context.xml" })
-public class DataSourceTest {
+public class MybatisTest {
 
     @Inject
-    private DataSource ds;
+    private SqlSessionFactory sqlFactory;
 
     @Test
-    public void testConnection() throws Exception {
-        Connection con;
+    public void testFactory() {
+        System.out.println(sqlFactory);
+    }
+
+    @Test
+    public void testSession() throws Exception {
+        SqlSession session;
         try {
-            con = ds.getConnection();
-            System.out.println(con);
+            session = sqlFactory.openSession();
+            System.out.println(session);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
